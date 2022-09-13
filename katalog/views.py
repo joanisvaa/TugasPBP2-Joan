b7a311d6ec33c48f88db3b41cef782ec98331994
joanisva <joan.isva@ui.ac.id>
2022-09-13 18:51:24 +0700

    Menyelesaikan Tugas 2

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+    data_barang_katalog = CatalogItem.objects.all()
+    context = {
+    'list_barang': data_barang_katalog,
+    'nama': 'Joan Isva Shanti Andrea',
+    'id': '2106707712'
+}
+    return render(request, "katalog.html", context)
+
